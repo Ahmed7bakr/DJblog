7475a341d2a715830bbb85459dd963608fbaab7b
Ahmed7bakr <a.bakr2472001@gmail.com>
2023-07-25 16:39:14 +0300

    add valid data to form

--- blog/urls.py
+++ blog/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from posts.views import post_list,post_detail,new_post
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blogs/', post_list),
+    path('blogs/<int:id>', post_detail),
+    path('blogs/new',new_post),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
+
+def post_list(request):
+    objects = Post.objects.all()
+    return render(request,'posts.html',{'posts':objects})
+
+def post_detail(request,id):
+    single = Post.objects.get(id=id)
+    return render (request,'post_detail.html',{'post':single})
+
+
+def new_post(request):
+    if request.method == 'POST':
+      form = PostForm(request.POST,request.FILES)
+      if form.is_valid():
+          form.save()
+    else:
+        form = PostForm()
+    return render(request,'new.html',{'form':form})
+
